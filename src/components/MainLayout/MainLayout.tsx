7f990b6a9421114e4153e83e765f0f56f3d3ca72
Maxim Paulenka <maxim.mephistos@gmail.com>
2022-08-29 21:56:35 +0300

    fix(task-2): fix site naming

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -8,9 +8,9 @@ import Header from "components/MainLayout/components/Header";
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {'Copyright © Maxim Li:br | '}
       <Link color="inherit" href="https://material-ui.com/">
-        My Store
+        Tea Store
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
